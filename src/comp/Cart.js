65e9aa22f49851a6e95db3abde4d9b6249c6853b
Noskina Olha <noskinaolga1@gmail.com>
2024-08-27 18:56:49 +0200

    Handle empty cart state and prevent errors when cart is undefined

--- src/comp/Cart.js
+++ src/comp/Cart.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import './cart.css'
-const Cart = ({cart}) => {
-  return (
-   <>
-   <div className='cart'>
-      <h3>#cart</h3>
-      <div className='cart-container'></div>
-   </div>
-   </>
-  )
-}
+import React from "react";
+import "./cart.css";
+import { Link } from "react-router-dom";
 
-export default Cart
+const Cart = ({  cart = []  }) => {
+  return (
+    <>
+      <div className="cart">
+        <h3>#cart</h3>
+        {cart.length === 0 && (
+          <>
+            <div className="empty-cart">
+              <h2>Your Shopping Cart is Empty</h2>
+              <Link to="/shop">
+                <button>Shop Now</button>
+              </Link>
+            </div>
+          </>
+        )}
+        <div className="cart-container"></div>
+      </div>
+    </>
+  );
+};
+
+export default Cart;
